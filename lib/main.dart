aa3baccbc505574a9aaf25b9616718051f00b0d4
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2024-06-04 18:08:07 +0530

    working with sdk setting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:chat_app_flutter/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -10,11 +17,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'FlutterChat',
         theme: ThemeData().copyWith(
           colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
+              seedColor: const Color.fromARGB(255, 17, 160, 179)),
         ),
-        home: Center());
+        home: const AuthScreen());
   }
 }
